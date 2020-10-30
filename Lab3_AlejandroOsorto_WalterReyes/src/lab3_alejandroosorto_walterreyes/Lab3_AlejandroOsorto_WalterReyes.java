27003bdd5c10bc3cf9dc09c9017c04c63d2478e0
walter reyes <walter.reyes@cambridgehn.com>
2020-10-30 13:56:45 -0600

    No nos da la actualizacion

--- Lab3_AlejandroOsorto_WalterReyes/src/lab3_alejandroosorto_walterreyes/Lab3_AlejandroOsorto_WalterReyes.java
+++ Lab3_AlejandroOsorto_WalterReyes/src/lab3_alejandroosorto_walterreyes/Lab3_AlejandroOsorto_WalterReyes.java
@@ -1,19 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab3_alejandroosorto_walterreyes;
 
-/**
- *
- * @author hp
- */
+
 public class Lab3_AlejandroOsorto_WalterReyes {
 
   
     public static void main(String[] args) {
-        System.out.println("Inicio");
+        
     }
     
 }
---
+++ Lab3_AlejandroOsorto_WalterReyes/src/lab3_alejandroosorto_walterreyes/Personas.java
@@ -0,0 +1,13 @@
+
+package lab3_alejandroosorto_walterreyes;
+
+import java.util.ArrayList;
+
+
+public class Personas {
+protected ArrayList id = new ArrayList();
+protected ArrayList Username = new ArrayList();
+protected String contraseña;
+protected String Correo;
+    
+}
