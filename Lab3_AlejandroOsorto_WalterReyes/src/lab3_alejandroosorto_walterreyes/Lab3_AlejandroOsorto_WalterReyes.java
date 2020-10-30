52a2cd2408f5224dc94cbba22c179af392fc49f5
walter reyes <walter.reyes@cambridgehn.com>
2020-10-30 13:27:47 -0600

    Start

---
+++ Lab3_AlejandroOsorto_WalterReyes/src/lab3_alejandroosorto_walterreyes/Lab3_AlejandroOsorto_WalterReyes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_alejandroosorto_walterreyes;
+
+/**
+ *
+ * @author hp
+ */
+public class Lab3_AlejandroOsorto_WalterReyes {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+    }
+    
+}
